1291a0dd10edf7109bcf45bf2eca0a9062db3b70
asus <aaa@aaa.aaa>
2020-10-23 18:29:31 +0500

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (i = 0; i < str.length; i++) {
+    for (j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] == bracketsConfig[j][0] && str[i + 1] == bracketsConfig[j][1]) {
+            str = str.replace(`${str[i]}${str[i + 1]}`, '');
+            i = i - 2;
+        }
+    }
+  }
+
+  return str === '';
 }
